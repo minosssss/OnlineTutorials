5790fabb31a8902df066a91fbd9cd2de0398acba
mh.lee <mh.lee239@gmail.com>
2022-03-03 23:42:51 +0900

    CircularNavMenu

---
+++ CircularNavMenu/script.js
@@ -0,0 +1,17 @@
+let menuToggle = document.querySelector('.toggle');
+let menu = document.querySelector('.menu');
+const list = document.querySelectorAll('li');
+
+function activeLink() {
+    list.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active')
+
+}
+list.forEach((item) => 
+item.addEventListener('click',activeLink))
+
+menuToggle.onclick = function() {
+    menu.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+}
